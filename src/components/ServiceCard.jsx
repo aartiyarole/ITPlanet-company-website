1d9cc3e597ba2af3bfed773582c305efd4387edf
Aarti <yaroleaarti@gmail.com>
2025-03-02 16:10:54 +0530

    cahnge anchor tag to Link

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export default function ServiceCard({ image, title, description, link }) {
   return (
     <div className="col-lg-4 p-3 wow animate fadeInDown" data-wow-delay="400ms" data-wow-duration="1500ms">
@@ -9,12 +9,12 @@ export default function ServiceCard({ image, title, description, link }) {
             <img src={image} className="img-fluid" alt={title} name={title} />
             <h2>{title}</h2>
             <p>{description}</p>
-            <a className="explore-btn" href={link}>
+            <Link className="explore-btn" to={link}>
               Learn More
               <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12">
                 <path fillRule="evenodd" clipRule="evenodd" d="M10.1865 1.06237L0 11.2484L0.751627 12L10.9376 1.81347V8.85645H12V0H3.14355V1.06237H10.1865Z" />
               </svg>
-            </a>
+            </Link>
           </center>
         </div>
       </div>
